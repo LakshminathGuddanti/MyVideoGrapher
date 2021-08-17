bf5068e5fb927989b8fd6464d1caef69b5d2c865
LakshminathGuddanti <lakshminath1214@gmail.com>
2021-08-17 21:01:19 +0530

    removed emailPassword

--- MyVideoGrapher/MyVideoGrapher/settings.py
+++ MyVideoGrapher/MyVideoGrapher/settings.py
@@ -139,5 +139,5 @@ LOGIN_REDIRECT_URL = 'home'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'lakshminath1214@gmail.com'
-EMAIL_HOST_PASSWORD = 'Lakshmi#1219'
+EMAIL_HOST_USER = '######'
+EMAIL_HOST_PASSWORD = '######'
